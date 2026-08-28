096fae528387505ff7df8743e3da067e9c69b5cf
Harshil <220130107108@gecg28.ac.in>
2026-08-28 17:29:50 +0530

    Complete State Master

--- routes/Master/cityMaster.js
+++ routes/Master/cityMaster.js
@@ -6,13 +6,9 @@ const cityMasterController = require("../../controllers/Master/CityMaster");
 
 
 router.post("/getCity", cityMasterController.getCity);
-
 router.post("/getCityById", cityMasterController.getCityById);
-
 router.post("/addEditCity", cityMasterController.addEditCity);
-
 router.post("/deleteCityById", cityMasterController.deleteCityById);
-
 router.post("/getCityHelp", cityMasterController.getCityHelp);
 
 
